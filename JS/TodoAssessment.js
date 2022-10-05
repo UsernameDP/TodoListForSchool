373c081f472a0403302e6a18794dcdc8f41850b8
Devin <112789891+UsernameDP@users.noreply.github.com>
2022-10-05 00:08:34 -0400

    Delete JS directory

--- JS/TodoAssessment.js
+++
@@ -1,6 +0,0 @@
-const display_main = document.getElementById("display_main");
-const display_todo_info_input = document.getElementById("display_todo_info_input");
-
-const display_todo_text = document.getElementById("display_todo_text");
-const display_todo_date = document.getElementById("display_todo_date");
-const display_todo_values = document.getElementById("display_todo_values")
--- JS/class.js
+++
@@ -1,9 +0,0 @@
-const class_main = document.getElementById("class_main");
-const class_sub = document.getElementById("class_sub");
-const class_append_location = document.getElementById("class_append_location");
-
-// Inputs for Class Info
-const class_period = document.getElementById("class_period");
-const class_name = document.getElementById("class_name");
-const class_addButton = document.getElementById("class_addButton");
-const class_close = document.getElementById("Class_Close");
